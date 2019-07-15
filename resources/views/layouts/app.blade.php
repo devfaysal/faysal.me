6714905c8a801c2b93a3c14d5ed27a6ec61e81b9
Faysal Ahamed <miufaysal@live.com>
2019-07-15 17:07:52 +0600

    Facebook og properties

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,11 +22,11 @@
     @isset($post)
         @include('posts.partials.facebook')
     @else
-        <meta name="description" content="Faysal Ahamed is a full-stack web developer who loves working with PHP, JavaScript, Laravel, Vue and WordPress. ">
-        <meta property="og:site_name" content="Faysal Ahamed">
-        <meta property="og:locale" content="en_US">
-        <meta property="og:description" content="Faysal Ahamed is a full-stack web developer who loves working with PHP, JavaScript, Laravel, Vue and WordPress.">
-        <meta property="og:url" content="{{ request()->fullUrl() }}">
+        <meta name='description' content="Faysal Ahamed is a full-stack web developer who loves working with PHP, JavaScript, Laravel, Vue and WordPress. ">
+        <meta property='og:site_name' content="Faysal Ahamed">
+        <meta property='og:locale' content="en_US">
+        <meta property='og:description' content="Faysal Ahamed is a full-stack web developer who loves working with PHP, JavaScript, Laravel, Vue and WordPress.">
+        <meta property='og:url' content="{{ request()->fullUrl() }}">
     @endisset
 </head>
 <body class="bg-gray-200 leading-tight">
--- resources/views/posts/partials/facebook.blade.php
+++ resources/views/posts/partials/facebook.blade.php
@@ -1,8 +1,8 @@
-<meta name="description" content="{{$post->excerpt}}">
-<meta property="og:url" content="{{URL::to($post->path())}}" />
-<meta property="og:type" content="article" />
-<meta property="og:title" content="{{$post->title}}" />
-<meta property="og:description" content="{{$post->excerpt}}" />
-<meta property="og:image" content="{{asset('storage/'.$post->image)}}" />
-<meta property="og:image:width" content="1000" />
-<meta property="og:image:height" content="500" />
+<meta name='description' content="{{$post->excerpt}}">
+<meta property='og:url' content="{{URL::to($post->path())}}" />
+<meta property='og:type' content="article" />
+<meta property='og:title' content="{{$post->title}}" />
+<meta property='og:description' content="{{$post->excerpt}}" />
+<meta property='og:image' content="{{asset('storage/'.$post->image)}}" />
+<meta property='og:image:width' content="1000" />
+<meta property='og:image:height' content="500" />
