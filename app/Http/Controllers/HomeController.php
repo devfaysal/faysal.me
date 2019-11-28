814747baa528ac0f81611ed35154ac5257b0886d
Faysal Ahamed <miufaysal@live.com>
2019-11-28 11:02:01 +0600

    Featured posts in homepage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('home');
+        $featured_posts = Post::where('featured', true)->get();
+        return view('home', [
+            'featured_posts' => $featured_posts
+        ]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,4 +3,18 @@
 @section('content')
     <h1 class="max-w-lg text-3xl md:text-4xl font-slab text-gray-700 font-extrabold leading-tight mb-1">Hi, I'm Faysal</h1>
     <p class="leading-relaxed text-lg text-gray-700">I'm a full-stack web developer who loves working with PHP, JavaScript, Laravel, Vue and WordPress. </p>
+
+    @if($featured_posts->count() > 0)
+    <div class="mt-10">
+        <div class="mb-2 p-2 bg-gray-700 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex" role="alert">
+            <span class="font-semibold px-5 flex-auto">Featured Blog Posts</span>
+        </div>
+        @foreach ($featured_posts as $post)
+            @include('posts.partials.post', [
+                'class' => 'mb-10',
+                'blog' => true
+            ])
+        @endforeach
+    </div>
+    @endif
 @endsection
